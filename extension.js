449584a651997a028194d67d803c8fa42fa987fc
Stefan Guggisberg <sguggisb@adobe.com>
2018-10-25 18:38:14 +0200

    htl syntax coloring (WIP)

--- extension.js
+++ extension.js
@@ -10,34 +10,32 @@
  * governing permissions and limitations under the License.
  */
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 // eslint-disable-next-line import/no-unresolved
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// called when extension is activated
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // eslint-disable-next-line no-console
-  console.log('Congratulations, your extension "project-helix" is now active!');
+  // register commands
+  const disposable = vscode.commands.registerCommand('extension.someHelixTask', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return; // No open text editor
+    }
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    // The code you place here will be executed every time your command is executed
+    const { selection } = editor;
+    const text = editor.document.getText(selection);
 
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
+    vscode.window.showInformationMessage(`Selected characters: ${text.length}`);
+    //vscode.window.showInformationMessage('Done Some Helix Task!');
   });
 
   context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
+// called when extension is deactivated
 function deactivate() {
+  // clean-up
 }
 exports.deactivate = deactivate;
